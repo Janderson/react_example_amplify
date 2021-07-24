648cd01a91f64d34f279325e0fe53d922be9a3df
Janderson de Farias Ferreira <ferreira_janderson@terceiros.sicredi.com.br>
2021-07-24 18:13:57 -0300

    add google auth into app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
+import config from './aws-exports';
+
+Amplify.configure(config);
 Amplify.configure(awsExports);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
