f8a914fce22b43edd2364051f964b91fd9f092a9
Janderson de Farias Ferreira <ferreira_janderson@terceiros.sicredi.com.br>
2021-07-24 16:51:56 -0300

    update app src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello JJ
+          Edit <code>src/App.js</code> and save to reload. Hello JJ. Auto Update
         </p>
         <a
           className="App-link"
